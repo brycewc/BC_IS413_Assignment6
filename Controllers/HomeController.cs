4da77cac0d61036e0fbb84be437a85f13af24d7d
Bryce Cindrich <bryce.cindrich@gmail.com>
2021-02-21 13:39:11 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using BC_IS413_Assignment6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BC_IS413_Assignment6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private IAmazonBooksRepository _repository;
+
+        public HomeController(ILogger<HomeController> logger, IAmazonBooksRepository repository)
+        {
+            _logger = logger;
+            _repository = repository; //Setting the IAmazonBooksRepository to the private attribute to send to Index
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.Books); //Passing the Books objects to Index view, ("Index", _repository.Books);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BC_IS413_Assignment6.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string AuthorFirst { get; set; }
+        public string AuthorMiddle { get; set; }
+        [Required]
+        public string AuthorLast { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        //Regular Expression to validate entered ISBN Numbers
+        [RegularExpression("^[0-9]{3}-[0-9]{10}$", ErrorMessage = "Must be a valid ISBN number in format and 10 or 13 digits")]
+        public string ISBN { get; set; }
+        [Required]
+        public string Classification { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [DataType(DataType.Currency, ErrorMessage = "Must be valid monetary value")]
+        public double Price { get; set; }
+        [Required]
+        public int NumOfPages { get; set; }
+    }
+}
